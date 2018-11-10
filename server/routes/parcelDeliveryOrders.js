925cbef3fe6e3a3c6b3c340f03254cf506c33f02
divCode77 <divineloves77@gmail.com>
2018-11-10 14:34:22 +0100

    feat(api): add /PUT /parcels/:parcelId/cancel endpoint
    -add /PUT /parcels/:parcelId/cancel endpoint
    -add test for  /PUT /parcels/:parcelId/cancel endpoint
    
    [Delivers #161858526]

--- server/routes/parcelDeliveryOrders.js
+++ server/routes/parcelDeliveryOrders.js
@@ -3,6 +3,7 @@ import express from 'express';
 import getParcelDeliveryOrders from '../controllers/getParcelDeliveryOrders';
 import getParcelDeliveryOrder from '../controllers/getParcelDeliveryOrder';
 import getUserOrders from '../controllers/getUserOrders';
+import cancelParcelDeliveryOrder from '../controllers/cancelParcelDeliveryOrder';
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get('/parcels', getParcelDeliveryOrders);
 router.get('/parcels/:parcelId', getParcelDeliveryOrder);
 router.get('/users/:userId/parcels', getUserOrders);
+router.put('/parcels/:parcelId/cancel', cancelParcelDeliveryOrder);
 
 export default router;
