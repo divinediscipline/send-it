6c3222938760a22980581cab0cbd0d5dd43d4c3f
divCode77 <divineloves77@gmail.com>
2018-11-14 15:56:31 +0100

    implementing admin page

--- UI/js/dashboard.js
+++ UI/js/dashboard.js
@@ -1,21 +1,13 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+const myFunction = () => {
   
   document.getElementById("myDropdown").style.display="block";
 }
 
 // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
+// window.onclick = (event) => {
 // if (!event.target.matches('.dropbtn')) {
 
-//   var dropdowns = document.getElementsByClassName("dropdown-content");
-//   var i;
-//   for (i = 0; i < dropdowns.length; i++) {
-//     var openDropdown = dropdowns[i];
-//     if (openDropdown.classList.contains('show')) {
-//       openDropdown.classList.remove('show');
-//     }
-//   }
-// }
+
 // }
