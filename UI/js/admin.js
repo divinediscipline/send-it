2db50358934e8c172078bb11fb4ab7d802de0810
divCode77 <divineloves77@gmail.com>
2018-11-16 11:25:39 +0100

    feat(client): add admin page
    - add page where admin can view all delivery orders
    - add control to view details of an order
    - add control to change status of an order
    - add control to change location of a n order
    
    [Delivers #161946237]

---
+++ UI/js/admin.js
@@ -0,0 +1,11 @@
+
+
+
+const  mouseOver = () => {
+  document.getElementById("options-box").style.display="block";
+}
+document.getElementById('table-icon').addEventListener("mouseover", mouseOver);
+
+window.onclick = () => {
+  document.getElementById("options-box").style.display="none";
+}
