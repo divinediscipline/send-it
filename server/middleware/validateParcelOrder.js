f387cdefb987923360ed1919c76f4cee096737d9
divCode77 <divineloves77@gmail.com>
2018-11-17 12:21:13 +0100

    fix(api routes): validate api routes appropriately
    - ensure unwanted characters flag errors in route
    - format error messages properly
    
    [Delivers #162030519]

--- server/middleware/validateParcelOrder.js
+++ server/middleware/validateParcelOrder.js
@@ -2,35 +2,23 @@ import Validator from 'validatorjs';
 
 const validateParcelOrder = (req, res, next) => {
   const data = {
-    sendersFirstName: req.body.sendersFirstName,
-    sendersLastName: req.body.sendersLastName,
-    sendersPhone: req.body.sendersPhone,
     parcelDescription: req.body.parcelDescription,
-    weightCategory: req.body.weightCategory,
-    price: req.body.price,
     pickUpLocation: req.body.pickUpLocation,
     destination: req.body.destination,
-    packageTransitTime: req.body.packageTransitTime,
     receiversFirstName: req.body.receiversFirstName,
     receiversLastName: req.body.receiversLastName,
     receiversEmail: req.body.receiversEmail,
-    receiversPhone: req.body.receiversPhone,
+    receiversPhoneNumber: req.body.receiversPhoneNumber,
   };
 
   const rules = {
-    sendersFirstName: 'required|min:3|string|max:20',
-    sendersLastName: 'required|min:3|string|max:20',
-    sendersPhone: 'required|numeric',
     parcelDescription: 'required|min:3|string|max:100',
-    weightCategory: 'required|min:3|string',
-    price: 'required|numeric',
     pickUpLocation: 'required|min:3|string|max:20',
     destination: 'required|min:3|string|max:20',
-    packageTransitTime: 'required|min:3|string|max:20',
-    receiversFirstName: 'required|min:3|string|max:20',
-    receiversLastName: 'required|min:3|string|max:20',
+    receiversFirstName: 'required|min:3|string|alpha|max:20',
+    receiversLastName: 'required|min:3|string|alpha|max:20',
     receiversEmail: 'required|email',
-    receiversPhone: 'required|numeric',
+    receiversPhoneNumber: 'required|numeric',
   };
 
   const validation = new Validator(data, rules);
