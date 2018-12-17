f0e23309639cbf3391e0eb4ef4f611718be29db6
divCode77 <divineloves77@gmail.com>
2018-12-17 20:26:04 +0100

    feat(order details page): consume change destination endpoint
    - consume PUT /parcels/:parcelId/destination
    - handle errors from wrong input on front end
    
    [Delivers #162086742]

--- UI/js/order-details.js
+++ UI/js/order-details.js
@@ -2,12 +2,48 @@ const token = localStorage.getItem('token');
 const urlParams = new URLSearchParams(window.location.search);
 const parcelId = urlParams.get('parcelid');
 let cancelButton;
+let changeDestination;
+
+const displayError1 = (bodyObject) => {
+  const errorElem = document.getElementById('error-element');
+  errorElem.style.visibility = 'visible';
+  errorElem.innerHTML = '';
+  errorElem.insertAdjacentHTML('beforeend', `<p style="background-color:#E53A40;font-size:1.5rem;color:#222;margin-bottom:5px;padding-left:8px;">${bodyObject.message}</p>`);
+};
+
+const submitNewDetails = async (form) => {
+  const data = JSON.stringify({
+    destination: form.destination.value,
+  });
+  const response = await fetch(`/api/v1/parcels/${parcelId}/destination`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-auth': token,
+    },
+    body: data,
+  });
+  const body = await response.json();
+  console.log('body', body);
+  if (response.status !== 200) {
+    displayError1(body);
+    return;
+  }
+  if (response.status === 200) {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+    const oldDestinationElem = document.getElementById('old-destination');
+    oldDestinationElem.innerHTML = body.data.destination;
+  }
+};
 
 const displayOrderDetails = (dataObject) => {
   if (dataObject.status === 'Cancelled') {
     cancelButton = '<a style="display:none;" id="cancel-btn" class="form-page__cancel-btn">Cancel order</a>';
+    changeDestination = '<button style="display:none;" id="change-destination" class="delivery-address__change-destination-btn" >Change destination</button>';
   } else {
     cancelButton = '<a id="cancel-btn" class="form-page__cancel-btn">Cancel order</a>';
+    changeDestination = '<button id="change-destination" class="delivery-address__change-destination-btn" >Change destination</button>';
   }
   const mainElem = document.getElementById('main-section');
   mainElem.insertAdjacentHTML('beforeend', `<div class="form-page">
@@ -44,7 +80,7 @@ const displayOrderDetails = (dataObject) => {
       <h4 class="delivery-address__heading">Delivery address</h4>
       <p class="delivery-address__field-group">
         <span class="delivery-address__label">Send to:</span>
-        <span class="delivery-address__field">${dataObject.destination}</span>
+        <span id="old-destination" class="delivery-address__field">${dataObject.destination}</span>
       </p>
       <p class="delivery-address__field-group">
         <span class="delivery-address__label">Receivers phone</span>
@@ -54,12 +90,29 @@ const displayOrderDetails = (dataObject) => {
         <span class="delivery-address__label">Receivers email</span>
         <span class="delivery-address__field">${dataObject.receiversemail}</span>
       </p>
+      ${changeDestination}
     </div>
     <div class="form-page__btns">
         <a href="./dashboard.html" class="form-page__view-btn ">View all orders</a>
         ${cancelButton}
     </div>
     
+    <!-- The Modal -->
+    <div id="myModal" class="delivery-address__modal">
+    
+      <!-- Modal content -->
+      <div class="delivery-address__modal-content">
+        <span class="delivery-address__close-btn">&times;</span>
+        <form id="new-destination-form" class="modal-form-group">
+          <div id="error-element" style="visibility:hidden;">error placeholder</div>
+          <label for="p">Enter your new destination details</label>
+          <input class="delivery-address__new-destination" type="text" name="destination" placeholder="New destination" required>
+          <button id="submit-destination" type="submit" class="delivery-address__change-destination-btn">Submit</button>
+        </form>
+        
+      </div>
+    
+    </div>
 </div>`);
 
   const cancelBtnElem = document.getElementById('cancel-btn');
@@ -75,8 +128,26 @@ const displayOrderDetails = (dataObject) => {
     if (response.status === 200) {
       const statusElem = document.getElementById('status');
       statusElem.innerHTML = body.data.status;
-      cancelBtnElem.style.display='none';
+      cancelBtnElem.style.display = 'none';
     }
+  });
+
+  // display the modal
+  const modal = document.getElementById('myModal');
+  const btn = document.getElementById('change-destination');
+  const span = document.getElementsByClassName('delivery-address__close-btn')[0];
+  btn.onclick = () => {
+    modal.style.display = 'block';
+  };
+  span.onclick = () => {
+    modal.style.display = 'none';
+  };
+
+  // update destination
+  const newDestinationForm = document.getElementById('new-destination-form');
+  newDestinationForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    submitNewDetails(newDestinationForm);
   });
 };
 
